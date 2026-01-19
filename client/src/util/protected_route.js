9df367dc692635e9da7d610b06f66b46229c4f61
Daniel Hernandez <danielmark252525@GMAIL.COM>
2026-01-18 19:39:45 -0500

    Migrate frontend to Vite + Apollo Client 3.x + React 18
    - Replace Create React App with Vite 6.x
    - Upgrade Apollo Client from 2.x to 3.12 (@apollo/client)
    - Upgrade React from 16.x to 18.x with createRoot API
    - Upgrade react-router-dom from 5.x to 7.x
    - Replace deprecated graphql-tag with gql from @apollo/client
    - Update route_util.js and protected_route.js to use useQuery hook
    - Update App.js with Routes/Route components (React Router v7)
    - Replace cache.writeData with reactive variables (makeVar)
    - Add ESLint flat config
    
    Note: Some components still import from react-apollo and need
    to be updated to use @apollo/client hooks instead.

--- client/src/util/protected_route.js
+++ client/src/util/protected_route.js
@@ -1,49 +1,20 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { Query } from "react-apollo";
+import { Navigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 import Queries from "../graphql/queries";
+
 const { IS_LOGGED_IN } = Queries;
 
-// our route switches on routeType
-const ProtectedRoute = ({
-  component: Component,
-  path,
-  exact,
-  routeType,
-  ...rest
-}) => (
-    <Query query={IS_LOGGED_IN}>
-      {({ data }) => {
-        // if the route type is "" then this route will only render if the
-        // user is logged in
-        if (routeType === "") {
-          return (
-            <Route
-              path={path}
-              exact={exact}
-              render={props =>
-                data.isLoggedIn ? <Component {...props} /> : <Redirect to="/" />
-              }
-            />
-          );
-        } else {
-          // otherwise this will be a protected route which will only
-          // render the component if the user is logged in
-          return (
-            <Route
-              {...rest}
-              render={props =>
-                data.isLoggedIn ? (
-                  <Component {...props} />
-                ) : (
-                    <Redirect to="/login" />
-                  )
-              }
-            />
-          );
-        }
-      }}
-    </Query>
-  );
+const ProtectedRoute = ({ element }) => {
+  const { data, loading } = useQuery(IS_LOGGED_IN);
+
+  if (loading) return null;
+
+  // This route will only render if the user is logged in
+  if (data?.isLoggedIn) {
+    return element;
+  }
+  return <Navigate to="/" replace />;
+};
 
 export default ProtectedRoute;
