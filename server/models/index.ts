733ace7e4557afde5e83436d3e974d481a4c3eef
Daniel Hernandez <danielmark252525@GMAIL.COM>
2026-01-24 20:33:45 -0500

    Revert "Phase 39: Waitlist Management System (#50)"
    This reverts commit 4481fef78133991d35ef03cb54ccecefd3db132d.

--- server/models/index.ts
+++ server/models/index.ts
@@ -2,13 +2,25 @@ import User from './User';
 import Animal from './Animal';
 import Application from './Application';
 import Shelter from './Shelter';
-import Waitlist from './Waitlist';
+import Foster from './Foster';
+import SavedSearch from './SavedSearch';
+import ApplicationTemplate from './ApplicationTemplate';
+import ActivityLog from './ActivityLog';
+import TerminalReader from './TerminalReader';
+import Message from './Message';
+import Volunteer from './Volunteer';
 
-export { User, Animal, Application, Shelter, Waitlist };
+export { User, Animal, Application, Shelter, Foster, SavedSearch, ApplicationTemplate, ActivityLog, TerminalReader, Message, Volunteer };
 
 // Also import models to ensure they are registered with Mongoose
 import './User';
 import './Animal';
 import './Application';
 import './Shelter';
-import './Waitlist';
+import './Foster';
+import './SavedSearch';
+import './ApplicationTemplate';
+import './ActivityLog';
+import './TerminalReader';
+import './Message';
+import './Volunteer';
